4639f65f269a762c9403dde9365b61c27c3ddb71
Ryan Kim <iawal86@gmail.com>
2020-09-14 15:40:34 -0700

    created search page

--- src/views/SearchPage.js
+++ src/views/SearchPage.js
@@ -28,6 +28,60 @@ function SearchPage() {
           price="$30 / night"
           total="$143 total"
         />
+        <SearchResult
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+          location="Private room in center of Los Angeles"
+          title="Stay at this spacious Edwardian House"
+          description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
+          star={4.73}
+          price="$30 / night"
+          total="$143 total"
+        />
+        <SearchResult
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+          location="Private room in center of Los Angeles"
+          title="Stay at this spacious Edwardian House"
+          description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
+          star={4.73}
+          price="$30 / night"
+          total="$143 total"
+        />
+        <SearchResult
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+          location="Private room in center of Los Angeles"
+          title="Stay at this spacious Edwardian House"
+          description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
+          star={4.73}
+          price="$30 / night"
+          total="$143 total"
+        />
+        <SearchResult
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+          location="Private room in center of Los Angeles"
+          title="Stay at this spacious Edwardian House"
+          description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
+          star={4.73}
+          price="$30 / night"
+          total="$143 total"
+        />
+        <SearchResult
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+          location="Private room in center of Los Angeles"
+          title="Stay at this spacious Edwardian House"
+          description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
+          star={4.73}
+          price="$30 / night"
+          total="$143 total"
+        />
+        <SearchResult
+          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_wbPYTxQPMcBh7SPzLFActXnP3uhifeVT_g&usqp=CAU"
+          location="Private room in center of Los Angeles"
+          title="Stay at this spacious Edwardian House"
+          description="1 guest · 1 bedroom · 1 bed · 1.5 shared bathrooms · Wifi · Kitchen · Free parking · Washing Machine"
+          star={4.73}
+          price="$30 / night"
+          total="$143 total"
+        />
       </div>
     </>
   );

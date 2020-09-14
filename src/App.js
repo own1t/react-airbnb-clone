c89fdb17b6f3fa3c043574c3f4ec011b3b535085
Ryan Kim <iawal86@gmail.com>
2020-09-14 14:05:13 -0700

    created header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+// Views
+import Home from "./views/Home";
+
+// Components
+import Header from "./components/Header";
+
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
